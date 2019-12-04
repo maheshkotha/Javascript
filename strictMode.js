c74989ae9b90335c6db7d376d2b6913b1ce0a861
Mahesh Kotha <44569697+maheshkotha@users.noreply.github.com>
2019-12-04 19:42:04 +0530

    this is not refer to newly constructed object

--- strictMode.js
+++ strictMode.js
@@ -13,3 +13,19 @@ function supporTheProblem() {
 		console.log("Hello");
     }
 }
+function person(name) {
+	this.name = name;
+}
+let fed = person ("fed");
+console.log(name);
+// feb
+
+"use strict"
+function per(name) {
+	this.name = name;
+}
+let f = per('feb');
+console.log(name)
+
+// Uncaught TypeError: Cannot set property 'name' of undefined
+
