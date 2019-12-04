a6bedb5bc02e11ec65b015b8d641f640e1564d81
Mahesh Kotha <44569697+maheshkotha@users.noreply.github.com>
2019-12-04 19:14:57 +0530

    strict mode

---
+++ strictMode.js
@@ -0,0 +1,15 @@
+//function supporTheProblem() {
+//    "use strict"
+//    for (i = 0; i < 10; i++) {
+//		console.log("Hello")
+//    }
+//}
+
+//Uncatched ReferencedError:
+
+function supporTheProblem() {
+    "use strict"
+    for (let i = 0; i < 10; i++) {
+		console.log("Hello");
+    }
+}
